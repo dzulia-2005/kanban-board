5b686bcb90c0caa4808346af1df59f0c3701ec99
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2026-01-11 20:13:27 +0400

    feat: add getallInquiries and patchInquiry with delay function

---
+++ src/app/api/inquiries/[id]/route.ts
@@ -0,0 +1,29 @@
+import {Delay} from "@/lib/delay";
+import {InquiryPhase} from "../../../../../mockData/inquiriesData.type";
+import {Inquiries} from "../../../../../mockData/inquiriesData";
+import {NextResponse} from "next/server";
+
+export async function PATCH(
+    req:Request,
+    {params}:{params:{id:string}}
+    ){
+
+    await Delay();
+
+    const body = await req.json();
+    const {phase} = body as {phase:InquiryPhase};
+
+    const inquiry =  Inquiries.find(i=>i.id===params.id);
+    if(!inquiry){
+        return NextResponse.json(
+            {messages:"inquiries not found"},
+            {status:404}
+        )
+    }
+
+
+    inquiry.phase=phase;
+    inquiry.updatedAt=new Date().toISOString();
+
+    return NextResponse.json(inquiry);
+}
---
+++ src/app/api/inquiries/route.ts
@@ -0,0 +1,36 @@
+import {Delay} from "@/lib/delay";
+import {Inquiries} from "../../../../mockData/inquiriesData";
+import {NextResponse} from "next/server";
+
+export async function GET(req:Request){
+    await Delay();
+
+    const {searchParams}=new URL(req.url);
+    const client = searchParams.get("client");
+    const minValue=searchParams.get("minValue");
+    const from = searchParams.get("from");
+    const to = searchParams.get("to");
+
+
+    let result =[...Inquiries];
+
+    if(client){
+        result = result.filter((i)=>{
+            return i.clientName.toLowerCase().includes(client.toLowerCase());
+        })
+    }
+
+    if(minValue){
+        result=result.filter((i)=>i.potentialValue>=Number(minValue));
+    }
+
+    if(to){
+        result=result.filter((i)=>i.eventDate<=to);
+    }
+
+    if(from){
+        result=result.filter((i)=>i.eventDate>=from);
+    }
+
+    return NextResponse.json(result);
+}
