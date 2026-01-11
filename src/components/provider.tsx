b05854b523a43f9c82d4b35773d2cfdc7eb19207
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2026-01-11 21:53:58 +0400

    feat: wrap app with Provider containing KanbanContext and QueryProvider

---
+++ src/components/provider.tsx
@@ -0,0 +1,14 @@
+import {ReactNode} from "react";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {queryClient} from "@/lib/queryClient";
+import {KanbanProvider} from "@/context/kanbanContext";
+
+export function Providers({children}:{children:ReactNode}){
+    return (
+        <QueryClientProvider client={queryClient}>
+            <KanbanProvider>
+                {children}
+            </KanbanProvider>
+        </QueryClientProvider>
+    )
+}
